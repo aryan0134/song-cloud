1a2b004cae88aaf8b0ad72e0c869502908ce6148
Ayusha0148 <ayusha0148.be20@chitkara.edu.in>
2022-09-04 11:49:38 +0530

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Search from './pages/Search';
+import './App.css';
 import Player from './components/Player';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Player />
-      <Routes>
-        <Route path='/' />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Player />
+        <div className="container">
+          <Routes>
+            <Route path='/' exact element={ <Home /> } />
+            <Route path='/About' element={ <About /> } />
+            <Route path='/Search' element={ <Search /> } />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
