bd2ed76791825f0315ca5c32ffaebe5d68da7be6
Ayusha0148 <110373390+Ayusha0148@users.noreply.github.com>
2022-09-04 11:57:41 +0530

    Update player.js

--- client/src/components/player.js
+++ client/src/components/player.js
@@ -25,14 +25,4 @@ function Player(){
     </>
   )
 }
-function Slider(){
-    return(
-        <div className='slider-container'>
-            <div className='progress-bar-cover'></div>
-            <div className='thumb'></div>
-            <input type='range' step='0.01' className='range'/>
-        </div>
-    )
-}
-
 export default Player; 
